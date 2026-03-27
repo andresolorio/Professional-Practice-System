00538fa1f0c529aaf68b254ea723a955cd674658
andre <andre@Laptop-Andre>
2026-03-26 22:37:26 -0600

    FEAT: Separación por carpetas segun modelo de 3 capas

---
+++ src/main/java/mx/uv/lis/professionalpracticesystem/ProfessionalPracticeSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.uv.lis.professionalpracticesystem;
+
+/**
+ *
+ * @author andre
+ */
+public class ProfessionalPracticeSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
